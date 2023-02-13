6fb2b1f2212940175c5ffec9e248eaf750179b2d
5anji <gitlanm@gmail.com>
2023-02-13 19:09:21 +0200

    add lab1

---
+++ lab1/main.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+
+// variant 19
+
+class Grammar {
+    std::vector<std::string> VN;
+    std::vector<std::string> VT;
+    std::map<std::string, std::vector<std::string>> P;
+
+public:
+    Grammar() {
+        VN = {"S", "A", "B", "C"};
+        VT = {"a", "b"};
+
+        P["S"] = {"aA"};
+        P["A"] = {"bS", "aB"};
+        P["B"] = {"bC"};
+        P["C"] = {"aA", "b"};
+    }
+
+    std::vector<std::string>& get_VN() {
+        return VN;
+    }
+    std::vector<std::string>& get_VT() {
+        return VT;
+    }
+
+    std::map<std::string, std::vector<std::string>>& get_P() {
+        return P;
+    }
+    void generate_strings() {
+        int count = 0;
+        while (count < 5) {
+            std::string curr = "S";
+            std::string res = "";
+
+            while (std::find(VN.begin(), VN.end(), curr) != VN.end()) {
+                int random = rand() % P[curr].size();
+                res += P[curr][random];
+                curr = res.back();
+            }
+
+            if (std::find(VT.begin(), VT.end(), curr) != VT.end()) {
+                std::cout << res << std::endl;
+                count++;
+            }
+        }
+    }
+};
+
+class FiniteAutomaton {
+    std::map<std::pair<std::string, std::string>, std::string> transitions;
+    std::vector<std::string> states;
+    std::vector<std::string> symbols;
+    std::string start_state;
+    std::vector<std::string> final_states;
+
+public:
+    FiniteAutomaton() = default;
+    FiniteAutomaton(Grammar& gr) {
+        for (auto [key, value] : gr.get_P()) {
+            for (std::string v : value) {
+                transitions[make_pair(key, v.substr(0, 1))] = v.substr(1, 1);
+                states.push_back(key);
+                symbols.push_back(v.substr(0, 1));
+            }
+        }
+
+        start_state = "S";
+        final_states = gr.get_VN();
+    }
+
+    bool is_valid_string(std::string str) {
+        std::string curr = start_state;
+        for (auto&& c : str) {
+            std::string symbol(1, c);
+            if (std::count(symbols.begin(), symbols.end(), symbol) == 0) {
+                return false;
+            }
+
+            curr = transitions[std::make_pair(curr, symbol)];
+        }
+
+        return std::count(final_states.begin(), final_states.end(), curr) > 0;
+    }
+};
+
+int main() {
+    Grammar g;
+    // g.generate_strings();
+
+    FiniteAutomaton fa(g);
+    std::cout << fa.is_valid_string("a") << std::endl;
+    std::cout << fa.is_valid_string("B") << std::endl;
+    std::cout << fa.is_valid_string("ab") << std::endl;
+    std::cout << fa.is_valid_string("aba") << std::endl;
+    
+    return 0;
+}
